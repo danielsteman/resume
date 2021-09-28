e3f53dcdb846ff4895ace37504856472579076e6
Daniel Steman <danielsteman@daniels-mbp13.home>
2021-09-28 23:17:56 +0200

    added title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import Title from './components/Title/Title';
 
 // Response grid example:
 // https://medium.com/swlh/create-a-responsive-grid-with-react-flexbox-styled-components-f7a55f607480
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Title></Title>
     </div>
   );
 }
---
+++ frontend/src/components/Title/Title.tsx
@@ -0,0 +1,11 @@
+import './Title.scss'
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Hi, I'm Daniel</h1>
+    </div>
+  )
+}
+
+export default Title
