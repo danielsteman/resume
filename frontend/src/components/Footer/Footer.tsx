baadc611dbbddfb8441db90b8274a5a924c622f7
Daniel Steman <danielsteman@daniels-mbp13.home>
2021-10-26 23:55:07 +0200

    centered components, installed plotly and added footer component

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import './Footer.scss';
+
+const Footer = () => (
+  <div className="footerComponent">
+    Footer
+  </div>
+);
+
+export default Footer;
--- frontend/src/components/Projects/SpotifyProject/SpotifyPI.tsx
+++ frontend/src/components/Projects/SpotifyProject/SpotifyPI.tsx
@@ -1,3 +1,4 @@
+// import * as Plotly from 'plotly.js';
 import './SpotifyPI.scss';
 
 const SpotifyPI = () => (
@@ -5,3 +6,5 @@ const SpotifyPI = () => (
 );
 
 export default SpotifyPI;
+
+// check example: https://stackoverflow.com/questions/39084438/how-to-import-plotly-js-into-typescript
