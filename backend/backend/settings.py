fe5aa6f1ed435b1b501bfc96ecedd50e782231e0
Daniel Steman <danielsteman@daniels-mbp13.home>
2021-11-01 22:43:35 +0100

    Wagtail WIP

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -152,12 +152,9 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, 'static'),
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ()
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
