bbb817091dd206098176dc230fd5e866fa86031e
Daniel Steman <danielsteman@daniels-mbp13.home>
2021-11-04 20:55:40 +0100

    first wagtail model

--- backend/resume/models.py
+++ backend/resume/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
+
+class ResumePage(Page):
+    intro = models.CharField(max_length=250)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
