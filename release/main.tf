28ff5e2816a9287fe56e0ab39b7104e602e9e0fa
danielsteman <daniel-steman@live.nl>
2022-02-01 22:39:40 +0100

    added terraform configuration WIP

--- release/main.tf
+++ release/main.tf
@@ -1,14 +1,33 @@
 terraform {
-  backend "local" {
-    path = "terraform/terraform.tfstate"
+  backend "pg" {
+    conn_str = var.connection_string
+  }
+  required_providers {
+    heroku = {
+      source  = "heroku/heroku"
+      version = "~> 4.0"
+    }
   }
 }
 
-provider "heroku" {
-  email   = "daniel-steman@live.nl"
-  api_key = var.heroku_api_key
+resource "heroku_app" "resume" {
+  name = var.app_name
+  region = var.region
 }
 
-variable heroku_api_key {
-  type = string
+resource "heroku_addon" "database" {
+  app  = heroku_app.resume.name
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+resource "heroku_addon" "ssl" {
+  app  = heroku_app.resume.name
+  plan = "ssl"
+}
+
+resource "heroku_cert" "ssl_certificate" {
+  app               = heroku_app.resume.name
+  certificate_chain = file("server.crt")
+  private_key       = file("server.key")
+  depends_on        = ["heroku_addon.ssl"]
 }
---
+++ release/variables.tf
@@ -0,0 +1,13 @@
+variable app_name {
+  type    = string
+  default = "daniels-resume"
+}
+
+variable region {
+  type    = string
+  default = "us"
+}
+
+variable connection_string {
+  type = string
+}
